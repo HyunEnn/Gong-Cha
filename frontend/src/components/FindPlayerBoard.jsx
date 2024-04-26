bf4801569c4a1baac10a913c0d946ca1488c3534
우찬명 <chanmyeongwoo@naver.com>
2024-04-26 16:35:08 +0900

    Feat: 선수구해요 게시판 일부 구현

---
+++ frontend/src/components/FindPlayerBoard.jsx
@@ -0,0 +1,29 @@
+import { useFindPlayerBoardStore } from '@/stores/findPlayerBoardStore';
+
+function FindPlayerBoard() {
+    const { dummyFindPlayerList } = useFindPlayerBoardStore();
+    return (
+        <>
+            {/* dummy를 real로 변경 필요 */}
+            {dummyFindPlayerList.map((value, index) => (
+                <div key={index} className="grid grid-cols-5 gap-4 mt-4 text-center border border-solid rounded-lg">
+                    <div className="flex flex-col justify-center pl-2 text-xl font-pretendardBold">{value.time}</div>
+                    <div className="flex flex-col justify-center col-span-3">
+                        <div className="font-pretendardBold">{value.place}</div>
+                        <div className="text-xs font-pretendardRegular text-neutral-500">
+                            •{value.gender}&nbsp;&nbsp;•{value.inOrOut}&nbsp;&nbsp;•{value.level}
+                        </div>
+                    </div>
+                    <div className="flex flex-col justify-center pr-2">
+                        <div className="text-lg font-pretendardBold">
+                            {value.currentPeople}/{value.totalPeople}
+                        </div>
+                        <div className="text-sm font-pretendardRegular">{value.writer}</div>
+                    </div>
+                </div>
+            ))}
+        </>
+    );
+}
+
+export default FindPlayerBoard;
