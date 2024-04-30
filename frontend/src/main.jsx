f25961b56edf9c350a8f8224704bbca8a00f7dca
우찬명 <chanmyeongwoo@naver.com>
2024-04-30 14:58:35 +0900

    Feat: time picker 사용을 위한 태그 추가

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,11 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { LocalizationProvider } from '@mui/x-date-pickers';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import App from '@/router/App.jsx';
 import '@/global.css';
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />);
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <LocalizationProvider dateAdapter={AdapterDayjs}>
+        <App />
+    </LocalizationProvider>
+);
