66b5d4a0ecaa403aaa979f148e9fd78a642e0218
정인상 <jus44556@naver.com>
2024-05-02 21:00:08 +0900

    Fix: @SpringBootTest 주석

--- backend/src/test/java/com/b306/gongcha/BackendApplicationTests.java
+++ backend/src/test/java/com/b306/gongcha/BackendApplicationTests.java
@@ -3,7 +3,7 @@ package com.b306.gongcha;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+// @SpringBootTest
 class BackendApplicationTests {
 
     @Test
