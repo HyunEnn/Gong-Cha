ace5d0e627570e14aff2fa576561919ab7fa2d6d
박하윤 <maha97523@gmail.com>
2024-05-07 03:49:18 +0900

    Feat: 추가 이적시장 Entity, Dto
    이적시장 Entity, Dto 추가

--- backend/src/main/java/com/b306/gongcha/repository/SeekTeamRepository.java
+++
@@ -1,7 +0,0 @@
-package com.b306.gongcha.repository;
-
-import com.b306.gongcha.entity.SeekTeam;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SeekTeamRepository extends JpaRepository<SeekTeam, Long> {
-}
