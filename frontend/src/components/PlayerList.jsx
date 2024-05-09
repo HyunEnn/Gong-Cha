bef362634232072729690ef02fc8ca94c2b24d1d
Ray Oh <rayoh2098@gmail.com>
2024-05-09 15:34:39 +0900

    feat: 요일 시간 필터 수정

--- frontend/src/components/PlayerList.jsx
+++ frontend/src/components/PlayerList.jsx
@@ -330,13 +330,13 @@ function PlayerList() {
         <div className="player-list-container flex-col items-center absolute left-1/2 transform -translate-x-1/2 top-[30%] w-[85%] h-[calc(100rem)]">
 
         <div className="flex-end">
-             {/* <input 
+             <input 
                 type="text" 
                 placeholder="아이디를 입력하세요" 
                 value={filter} 
                 onChange={e => setFilter(e.target.value)} 
                 className="narrow-input"
-            />  */}
+            /> 
         </div>
             {filteredPlayers.map(renderPlayerItem)}
         </div>
