0457c2e7c8d992cf6427e884abd335c524f7a37a
Ray Oh <rayoh2098@gmail.com>
2024-05-09 10:10:16 +0900

    feat: 선수 리스트 토글 추가

--- frontend/dev-dist/sw.js
+++ frontend/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-b5f7729d'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.ddsncvdes7o"
+    "revision": "0.mcgip7ijkqg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
