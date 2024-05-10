be74f98e325d69b2cac78411e109d043705fe540
Ray Oh <rayoh2098@gmail.com>
2024-05-10 14:29:52 +0900

    feat: 선수리스트 모달창 수정

--- frontend/src/components/modals/PlayerModal.jsx
+++ frontend/src/components/modals/PlayerModal.jsx
@@ -4,23 +4,42 @@ function PlayerModal({ isOpen, onClose, player }) {
   if (!isOpen || !player) return null;
 
   return (
-    <div className="backdrop">
-      <div className="modal">
-        <div className="modal-content">
-          <h2>Player Information</h2>
-          <ul>
-            <li>Time: {player.data.startTime} - {player.data.endTime}</li>
-            <li>Name: {player.data.user_id}</li>
-            <li>Age: {player.data.age}</li>
-            <li>Position: {player.data.position}</li>
-            <li>Team: {player.data.team}</li>
-          </ul>
-          <button onClick={onClose} className="close-button">Close</button>
+    <div className="fixed inset-0 z-50 flex items-center justify-center overflow-y-auto">
+      <div className="relative w-auto max-w-3xl mx-auto my-6">
+        {/* Overlay */}
+        <div className="fixed inset-0 bg-black opacity-50"></div>
+
+        {/* Modal Content */}
+        <div className="relative z-50 bg-white rounded-lg shadow-lg">
+          {/* Modal Header */}
+          <div className="flex items-center justify-center p-5 border-b border-gray-300 rounded-t">
+            <h3 className="text-3xl font-semibold">선수 정보</h3>
+          </div>
+
+          {/* Modal Body */}
+          <div className="p-6">
+            <ul className="text-gray-600 text-lg leading-relaxed">
+              <li>시간: {player.data.start_time}~{player.data.end_time}</li>
+              <li>요일: {player.data.day_of_week}</li>
+              <li>이름: {player.data.user_id}</li>
+              <li>난이도: {player.data.difficulty}</li>
+              <li>Team: {player.data.team}</li>
+            </ul>
+          </div>
+
+          {/* Modal Footer */}
+          <div className="flex items-center justify-center p-6 border-t border-gray-300 rounded-b">
+            <button
+              className="text-white bg-gray-300 font-bold uppercase text-sm px-6 py-2 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+              onClick={onClose}
+            >
+              수락하기
+            </button>
+          </div>
         </div>
       </div>
     </div>
   );
 }
-
 
 export default PlayerModal;
