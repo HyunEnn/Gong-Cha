da9ed1f1836a5456a71aeeb9900f7d30c4493a69
우찬명 <chanmyeongwoo@naver.com>
2024-05-10 10:38:01 +0900

    Feat: 클럽 둘러보기 개발

--- frontend/src/pages/ClubPage.jsx
+++ frontend/src/pages/ClubPage.jsx
@@ -1,8 +1,24 @@
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
+
+import SearchClub from '@/components/SearchClub';
+import MyClub from '@/components/MyClub';
+
 function ClubPage() {
     return (
-        <>
-            <p>클럽페이지</p>
-        </>
+        <div className="px-2 mt-4">
+            <Tabs defaultValue="searchClub" className="w-[330px] mx-auto">
+                <TabsList className="flex justify-around">
+                    <TabsTrigger value="searchClub">클럽 둘러보기</TabsTrigger>
+                    <TabsTrigger value="myClub">나의 클럽 보기</TabsTrigger>
+                </TabsList>
+                <TabsContent value="searchClub">
+                    <SearchClub />
+                </TabsContent>
+                <TabsContent value="myClub">
+                    <MyClub />
+                </TabsContent>
+            </Tabs>
+        </div>
     );
 }
 
