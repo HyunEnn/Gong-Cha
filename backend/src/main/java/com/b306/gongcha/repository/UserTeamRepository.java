d1482ab554880526908db061e9e35a5ba75db2bc
박하윤 <maha97523@gmail.com>
2024-05-14 09:06:08 +0900

    Fix: 수정 코드 줄 추가
    엔터로 코드 띄어쓰기

--- backend/src/main/java/com/b306/gongcha/repository/UserTeamRepository.java
+++ backend/src/main/java/com/b306/gongcha/repository/UserTeamRepository.java
@@ -14,19 +14,27 @@ import java.util.Optional;
 public interface UserTeamRepository extends JpaRepository<UserTeam, Long> {
 
     Optional<UserTeam> findByTeamIdAndUserId(Long teamId, Long userId);
+
     // 승인 여부와 상관없이 신청한 모든 유저 정보 반환
     List<UserTeam> findAllByTeamId(Long teamId);
+
     List<UserTeam> findByUserId(Long userId);
+
     // 팀에 승인된 선수들만 보여주기 
     List<UserTeam> findAllByTeamIdAndPermitIsTrue(Long teamId);
+
     // 승인 대기 목록 선수들만 보여주기
     List<UserTeam> findAllByTeamIdAndPermitIsFalse(Long teamId);
+
     // 유저 id와 역할로 팀장 여부 확인
     UserTeam findByUserIdAndRole(Long userId, Role role);
+
     // 팀 id로 유저 정보 목록 불러오기
     @Query("select u from User u left outer join UserTeam ut on u.id = ut.user.id where ut.team.id = :teamId")
     List<User> findUsersByTeamId(Long teamId);
+
     // 팀 id로 유저 카드 목록을 불러오기
     @Query("select c from Card c left outer join UserTeam ut on c.user.id = ut.user.id where ut.team.id = :teamId")
     List<Card> findCardsByTeamId(Long teamId);
+
 }
