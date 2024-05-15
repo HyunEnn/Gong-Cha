484cce17bf549f6bd69bb84bc9b7fe0db712b568
박하윤 <maha97523@gmail.com>
2024-05-15 15:53:19 +0900

    Feat: 추가 @ApiResponse
    @ApiResponse 추가

--- backend/src/main/java/com/b306/gongcha/controller/TeamController.java
+++ backend/src/main/java/com/b306/gongcha/controller/TeamController.java
@@ -2,9 +2,16 @@ package com.b306.gongcha.controller;
 
 import com.b306.gongcha.dto.request.TeamRequest;
 import com.b306.gongcha.dto.response.CommonResponse;
+import com.b306.gongcha.dto.response.TeamResponse;
+import com.b306.gongcha.dto.response.UserTeamResponse;
+import com.b306.gongcha.exception.CustomException;
+import com.b306.gongcha.exception.ErrorCode;
 import com.b306.gongcha.service.TeamService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
@@ -26,10 +33,7 @@ public class TeamController {
             summary = "팀 전체 조회",
             description = "팀 목록 전체 조회"
     )
-    @ApiResponse(
-            responseCode = "200",
-            description = "팀 목록 전체 조회 완료"
-    )
+    @ApiResponse(responseCode = "200", description = "팀 목록 전체 조회 완료", content = @Content(schema = @Schema(implementation = TeamResponse.class)))
     @GetMapping("")
     public ResponseEntity<CommonResponse> getAllTeams(
             @PageableDefault(sort = "createdAt", direction = Sort.Direction.DESC, size = 10) Pageable pageable) {
@@ -45,9 +49,11 @@ public class TeamController {
             summary = "팀 상세 조회",
             description = "팀 상세 정보 조회"
     )
-    @ApiResponse(
-            responseCode = "200",
-            description = "팀 상세 정보 조회 완료"
+    @ApiResponses(
+            value = {
+                    @ApiResponse(responseCode = "200", description = "팀 상세 정보 조회 완료", content = @Content(schema = @Schema(implementation = TeamResponse.class))),
+                    @ApiResponse(responseCode = "404", description = "존재하지 않는 팀입니다", content = @Content(schema = @Schema(implementation = ErrorCode.class)))
+            }
     )
     @GetMapping("/{teamId}")
     public ResponseEntity<CommonResponse> getTeam(@PathVariable Long teamId) {
@@ -62,9 +68,11 @@ public class TeamController {
             summary = "팀의 팀원 조회",
             description = "팀의 팀원들 조회"
     )
-    @ApiResponse(
-            responseCode = "200",
-            description = "팀원들이 정상적으로 조회되었습니다."
+    @ApiResponses(
+            value = {
+                    @ApiResponse(responseCode = "200", description = "팀원들이 정상적으로 조회되었습니다.", content = @Content(schema = @Schema(implementation = UserTeamResponse.class))),
+                    @ApiResponse(responseCode = "404", description = "존재하지 않는 팀입니다", content = @Content(schema = @Schema(implementation = ErrorCode.class)))
+            }
     )
     @GetMapping("/{teamId}/teamUsers")
     public ResponseEntity<CommonResponse> getTeamUsers(@PathVariable Long teamId) {
@@ -80,9 +88,11 @@ public class TeamController {
             summary = "팀 생성",
             description = "유저가 팀 생성"
     )
-    @ApiResponse(
-            responseCode = "200",
-            description = "팀 생성 완료"
+    @ApiResponses(
+            value = {
+                    @ApiResponse(responseCode = "200", description = "팀 생성 완료", content = @Content(schema = @Schema(implementation = TeamResponse.class))),
+                    @ApiResponse(responseCode = "404", description = "존재하지 않는 유저입니다", content = @Content(schema = @Schema(implementation = ErrorCode.class)))
+            }
     )
     @PostMapping("")
     public ResponseEntity<CommonResponse> createTeam(@RequestBody TeamRequest teamRequest) {
@@ -97,9 +107,11 @@ public class TeamController {
             summary = "팀 정보 수정",
             description = "팀장이 팀 정보 수정"
     )
-    @ApiResponse(
-            responseCode = "200",
-            description = "팀 정보 수정 완료"
+    @ApiResponses(
+            value = {
+                    @ApiResponse(responseCode = "200", description = "팀 정보 수정 완료", content = @Content(schema = @Schema(implementation = TeamResponse.class))),
+                    @ApiResponse(responseCode = "404", description = "존재하지 않는 팀입니다", content = @Content(schema = @Schema(implementation = ErrorCode.class)))
+            }
     )
     @PatchMapping("/{teamId}")
     public ResponseEntity<CommonResponse> updateTeam(@PathVariable Long teamId, @RequestBody TeamRequest teamRequest) {
@@ -114,9 +126,11 @@ public class TeamController {
             summary = "팀 삭제",
             description = "팀장이 팀원이 없는 본인의 팀 삭제"
     )
-    @ApiResponse(
-            responseCode = "200",
-            description = "팀 삭제 완료"
+    @ApiResponses(
+            value = {
+                    @ApiResponse(responseCode = "200", description = "팀 삭제 완료"),
+                    @ApiResponse(responseCode = "404", description = "존재하지 않는 유저입니다", content = @Content(schema = @Schema(implementation = ErrorCode.class)))
+            }
     )
     @DeleteMapping("/{teamId}")
     public ResponseEntity<CommonResponse> deleteTeam(@PathVariable Long teamId) {
@@ -131,9 +145,11 @@ public class TeamController {
             summary = "선수 -> 팀 참가 요청",
             description = "선수가 팀에게 팀 참여 요청"
     )
-    @ApiResponse(
-            responseCode = "200",
-            description = "팀 참여 신청 완료"
+    @ApiResponses(
+            value = {
+                    @ApiResponse(responseCode = "200", description = "팀 참여 신청 완료", content = @Content(schema = @Schema(implementation = UserTeamResponse.class))),
+                    @ApiResponse(responseCode = "404", description = "존재하지 않는 팀입니다", content = @Content(schema = @Schema(implementation = ErrorCode.class)))
+            }
     )
     @PostMapping("/{teamId}/{userId}")
     public ResponseEntity<CommonResponse> requestTeam(@PathVariable Long teamId, @PathVariable Long userId) {
@@ -148,9 +164,11 @@ public class TeamController {
             summary = "팀이 받은 요청 목록 조회",
             description = "팀장이 팀이 받은 요청 목록 조회"
     )
-    @ApiResponse(
-            responseCode = "200",
-            description = "팀이 받은 요청 목록 조회 성공"
+    @ApiResponses(
+            value = {
+                    @ApiResponse(responseCode = "200", description = "팀이 받은 요청 목록 조회", content = @Content(schema = @Schema(implementation = UserTeamResponse.class))),
+                    @ApiResponse(responseCode = "404", description = "존재하지 않는 팀입니다", content = @Content(schema = @Schema(implementation = ErrorCode.class)))
+            }
     )
     @GetMapping("/request/{teamId}")
     public ResponseEntity<CommonResponse> getTeamRequest(@PathVariable Long teamId) {
@@ -165,9 +183,11 @@ public class TeamController {
             summary = "선수의 요청 승인",
             description = "팀장이 선수의 참여 요청을 승인"
     )
-    @ApiResponse(
-            responseCode = "200",
-            description = "선수 요청 승인"
+    @ApiResponses(
+            value = {
+                    @ApiResponse(responseCode = "200", description = "선수 요청 승인", content = @Content(schema = @Schema(implementation = UserTeamResponse.class))),
+                    @ApiResponse(responseCode = "404", description = "존재하지 않는 팀입니다", content = @Content(schema = @Schema(implementation = ErrorCode.class)))
+            }
     )
     @PatchMapping("/{teamId}/{userId}")
     public ResponseEntity<CommonResponse> acceptTeam(@PathVariable Long teamId, @PathVariable Long userId) {
@@ -182,9 +202,11 @@ public class TeamController {
             summary = "선수의 요청 거절",
             description = "팀장이 선수의 참여 요청을 거절"
     )
-    @ApiResponse(
-            responseCode = "200",
-            description = "선수 요청 거절"
+    @ApiResponses(
+            value = {
+                    @ApiResponse(responseCode = "200", description = "선수 요청 거절"),
+                    @ApiResponse(responseCode = "404", description = "존재하지 않는 요청입니다", content = @Content(schema = @Schema(implementation = ErrorCode.class)))
+            }
     )
     @DeleteMapping("/{teamId}/{userId}")
     public ResponseEntity<CommonResponse> rejectTeam(@PathVariable Long teamId, @PathVariable Long userId) {
@@ -199,9 +221,11 @@ public class TeamController {
             summary = "팀장이 선수 모집 종료",
             description = "팀장이 게시판에서 선수 모집을 종료함"
     )
-    @ApiResponse(
-            responseCode = "200",
-            description = "팀장이 선수 모집 종료 성공"
+    @ApiResponses(
+            value = {
+                    @ApiResponse(responseCode = "200", description = "팀장이 선수 모집 종료"),
+                    @ApiResponse(responseCode = "404", description = "존재하지 않는 팀입니다", content = @Content(schema = @Schema(implementation = ErrorCode.class)))
+            }
     )
     @PatchMapping("/close/{teamId}")
     public ResponseEntity<CommonResponse> endTeamRequest(@PathVariable Long teamId) {
