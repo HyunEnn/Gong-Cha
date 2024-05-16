f08c8ddefabac0615b853fe57d7a313d0f8b324e
우찬명 <chanmyeongwoo@naver.com>
2024-05-16 15:47:03 +0900

    Feat: axios api 작성

--- frontend/src/apis/util/commons.js
+++ frontend/src/apis/util/commons.js
@@ -9,6 +9,7 @@ function serverAxios() {
         withCredentials: true,
         headers: {
             'Content-Type': 'application/json;charset=utf-8',
+            Authorization: `Bearer eyJhbGciOiJIUzI1NiJ9.eyJjYXRlZ29yeSI6ImFjY2VzcyIsInVzZXJJZCI6MywidXNlckluZm8iOiJrYWthbyAzNDgzNDI4MDE4Iiwicm9sZSI6IlJPTEVfVVNFUiIsImlhdCI6MTcxNTc4OTAzMCwiZXhwIjoxNzE1NzkyNjMwfQ.GxYbGd5JfNo5NNcEUv4-idcYv3iWm64gbNwUkR5sT2E`,
         },
     });
 
