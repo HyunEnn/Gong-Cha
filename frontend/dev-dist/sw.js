0cab9bec0a5b8ce8ab09fe6ba81e13b0c2ba1e66
우찬명 <chanmyeongwoo@naver.com>
2024-05-16 15:50:49 +0900

    Chore: 패키지 매니저 수정, 그 외 기타 수정

--- frontend/dev-dist/sw.js
+++ frontend/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-b5f7729d'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.4234rii6rag"
+    "revision": "0.umljn5m25n"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
