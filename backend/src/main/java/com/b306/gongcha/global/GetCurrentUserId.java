b265e04168ae7ba07780a089b30610818b386396
김현준 <denny10002@naver.com>
2024-05-16 10:51:33 +0900

    fix: 로그인 access 처리 방식 변경

--- backend/src/main/java/com/b306/gongcha/global/GetCurrentUserId.java
+++ backend/src/main/java/com/b306/gongcha/global/GetCurrentUserId.java
@@ -1,11 +1,27 @@
 package com.b306.gongcha.global;
 
 import com.b306.gongcha.dto.response.CustomOAuth2User;
+import com.b306.gongcha.repository.UserRepository;
+import io.jsonwebtoken.Jwts;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+import java.nio.charset.StandardCharsets;
 
+@Slf4j
 public class GetCurrentUserId {
 
+    private final UserRepository userRepository;
+
+    private final SecretKey secretKey;
+
+    public GetCurrentUserId(UserRepository userRepository, @Value("${spring.jwt.secret}") String secret) {
+        this.userRepository = userRepository;
+        secretKey = new SecretKeySpec(secret.getBytes(StandardCharsets.UTF_8), Jwts.SIG.HS256.key().build().getAlgorithm());
+    }
     public static Long currentUserId() {
         // SecurityContext에서 Authentication 객체 가져오기
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
@@ -19,4 +35,5 @@ public class GetCurrentUserId {
         }
         return userId;
     }
+
 }
