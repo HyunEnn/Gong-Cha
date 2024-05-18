a8968abe8147cd14c05ccb29ff457f3e55cb4c18
박하윤 <maha97523@gmail.com>
2024-05-19 02:19:12 +0900

    Feat: 추가 유저가 플레이한 경기 내역 목록 조회
    유저 id로 해당 유저가 플레이했던 경기들의 목록 조회

--- backend/src/main/java/com/b306/gongcha/repository/MatchingAskRepository.java
+++ backend/src/main/java/com/b306/gongcha/repository/MatchingAskRepository.java
@@ -1,6 +1,8 @@
 package com.b306.gongcha.repository;
 
+import com.b306.gongcha.entity.Matching;
 import com.b306.gongcha.entity.MatchingAsk;
+import io.lettuce.core.dynamic.annotation.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
@@ -23,5 +25,9 @@ public interface MatchingAskRepository extends JpaRepository<MatchingAsk, Long> 
 
     @Query("select count(ma) from MatchingAsk ma where ma.permit = true and ma.versusTeamId = (select ut.team.id from UserTeam ut where ut.team.status = '경기종료' and ut.user.id = (select u.id from User u where u.id = :userId))")
     int countAllByVersusTeamIdAndStatus(Long userId);
+
+    @Query("select ma from MatchingAsk ma where ma.matching.matchingTeamId = (select ut.team.id from UserTeam ut where ut.team.status = '경기종료' and ut.user.id = :userId)" +
+            "or ma.versusTeamId = (select ut.team.id from UserTeam ut where ut.team.status = '경기종료' and ut.user.id = :userId)")
+    List<MatchingAsk> findAllByUserIdAndStatus(@Param("userId") Long userId);
 
 }
